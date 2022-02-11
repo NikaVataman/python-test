3dd04664ca8ed90da89f2c5f092a26c9d4656b8e
Nika <vnika2607@gmail.com>
2022-02-11 21:55:39 +0200

    bla

---
+++ nika.py
@@ -0,0 +1,36 @@
+from turtle import *
+color("red")
+width(5)
+x = 150
+y = 0
+goto(x,y)
+right(90)
+x2 = x
+y2 = x + 40
+goto(x2,y2)
+right(90)
+x3 = 0
+y3 = y2
+goto(x3,y3)
+left(90)
+x4 = x3
+y4 = 0
+goto(x4,y4)
+
+color("purple")
+width(7)
+x5 = 0
+y5 = x - 90
+goto(x5,y5)
+left(90)
+x6 = -100
+y6 = x - 90
+goto(x6,y6)
+right(90)
+x7 = -100
+y7 = x - 150
+goto(x7,y7)
+right(90)
+x8 = 0
+y8 = x8
+goto(x8,y8)
